a9a556ff5852adfe4e9c037c4f935f6f9a7761d4
charles-xiao <330239858@qq.com>
2017-04-17 00:45:27 +0800

    Update create_pxe_dir.sh
    add coreos.yml

--- create_pxe_dir.sh
+++ create_pxe_dir.sh
@@ -7,6 +7,8 @@ wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.
 wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
 wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.0
 wget https://github.com/Charles-x/pxeserver/tree/master/pxe/gpxelinux.0
+wget https://github.com/Charles-x/pxeserver/tree/master/pxe/cloud-init .yml
+wget https://github.com/Charles-x/pxeserver/tree/master/pxe/cloud-config.yml
 cd $WORKDIR/pxe/pxelinux.cfg
 wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.cfg/default
 echo "----------------------you should to mount pxe dir to docker /pxe dir-----------------------------------"
