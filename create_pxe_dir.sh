cf624511e98c74804a809dd452972bdd30d2f102
charles-xiao <330239858@qq.com>
2017-04-16 21:49:58 +0800

    Create create_pxe_dir.sh

---
+++ create_pxe_dir.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+WORKDIR=/tmp
+cd $WORKDIR
+mkdir -p $WORKDIR/pxe/tftpboot/pxelinux.cfg/
+cd $WORKDIR/pxe/tftpboot
+wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.0
+cd $WORKDIR/pxe/tftpboot/pxelinux.cfg
+wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.cfg/default
