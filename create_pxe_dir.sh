b8c9aa8525ef48144addedd10a3dbcd613c58486
charles-xiao <330239858@qq.com>
2017-04-16 23:40:10 +0800

    Update create_pxe_dir.sh
    v1.0

--- create_pxe_dir.sh
+++ create_pxe_dir.sh
@@ -6,5 +6,7 @@ cd $WORKDIR/pxe/tftpboot
 wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
 wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
 wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.0
+wget https://github.com/Charles-x/pxeserver/tree/master/pxe/gpxelinux.0
 cd $WORKDIR/pxe/tftpboot/pxelinux.cfg
 wget https://github.com/Charles-x/pxeserver/tree/master/pxe/pxelinux.cfg/default
+echo "you should to mount pxe dir to docker /pxe dir"
